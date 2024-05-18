a4c32238e90f643c99eb8a334f01026989e85a76
Caio J0rdan <caio10jordan@alu.ufc.br>
2024-05-18 12:24:45 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using DesafioPOO.Models;
+// Implementado !!!
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+/*------------------Main de teste das classes---------------------*/
+
+// Criação dos objetos
+Nokia nk = new Nokia("91234-5678","Silver","87843284872",64);
+
+Iphone ip = new Iphone("98765-4321","Gold","89747347467",128);
+
+
+//Teste das funções de Ligação
+
+nk.Ligar();
+ip.ReceberLigacao();
+
+
+// teste da função InstalarApp
+
+nk.InstalarAplicativo("Facebook");
+
+ip.InstalarAplicativo("Instagram");
+
+
+
+
+
